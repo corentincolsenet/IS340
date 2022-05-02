7e799444104d168360a7acb59d9df570b75d2a2c
Corentin Colsenet <corentincolsenet@Corentins-MacBook-Pro.local>
2022-05-02 00:45:54 -0700

    remove ';'

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from player import Player, PLAYER_TYPE_AI, PLAYER_TYPE_REAL
 
 def main():
     if checkArguments(argv) == 84:
-        return 84;
+        return 84
     players = generatePlayers(argv)
-    # game = Game(players[0], players[1]);
-    return 0;
+    # game = Game(players[0], players[1])
+    return 0
 
 if __name__ == "__main__":
     main()
